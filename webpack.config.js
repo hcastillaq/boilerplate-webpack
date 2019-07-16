ecc18855f46bb781dfc222083d88304ac5dc02b9
Hernan Castilla <hcastillaq@gmail.com>
2019-07-16 12:56:40 -0500

    configuracion basica con webpack y babel

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const smp = new SpeedMeasurePlugin();
+
+module.exports = smp.wrap({
+	target: "web",
+	mode: "development",
+	entry:
+	{
+		app: "./index.js"
+	},
+	output:
+	{
+		filename: 'bundle.js',
+		path: __dirname + '/build'
+	},
+	module:
+	{
+		rules:
+			[
+				{
+					test: /\.(ts|tsx)$/,
+					loader: 'babel-loader',
+				}
+			]
+	},
+	resolve:
+	{
+		extensions: ['.js', '.jsx', '.tsx', '.ts', '.json']
+	},
+	plugins:[
+		new BundleAnalyzerPlugin()
+	]
+});
